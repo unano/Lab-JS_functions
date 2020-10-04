5ec1eb4a16acb1b9601c129447f9ec5e95dca61d
Jiaqi Gu <919659233@qq.com>
2020-10-04 19:53:09 +0100

    Exercise 1

--- exercise6.js
+++ exercise6.js
@@ -1,10 +1,10 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
   .then(response => response.json())
   .then(json => {
-     const completed = json.reduce( (total, todo) => {
-         if (todo.completed){
-             
-         };
+     const completed = json.reduce((total, todo) => {
+       if (todo.completed) {
+         total[todo.userId] === undefined ? (total[todo.userId] = 1) : (total[todo.userId] += 1);
+        };
          return total;
     }, {});
       console.log(completed);
